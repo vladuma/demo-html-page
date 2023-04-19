1825d023bfdb171f3381b01dd9b5bcd9e985a1c6
Vlad <vlad0duma@gmail.com>
2023-04-19 21:26:20 +0300

    add: load app wioth script

---
+++ script.js
@@ -0,0 +1,31 @@
+// const APP_URL = 'https://ca-immigration-chat-bot.netlify.app';
+const APP_URL = "http://localhost:3000";
+fetch(`${APP_URL}`)
+  .then((response) => response.text())
+  .then((html) => {
+    console.log(html);
+    processAppHTML(html);
+  })
+  .catch((error) => {
+    console.error("Failed to fetch Next.js app content:", error);
+  });
+
+function appendHost(str) {
+  return str.replace(window.location.origin, APP_URL);
+}
+
+function processAppHTML(html) {
+  const container = document.createElement("div");
+  container.innerHTML = html;
+
+  const scripts = container.querySelectorAll("script");
+  scripts.forEach((script) => {
+    const newScript = document.createElement("script");
+    newScript.src = appendHost(script.src);
+    document.head.appendChild(newScript);
+  });
+  
+  const rootDiv = container.querySelector("#root");
+  
+  document.body.appendChild(rootDiv);
+}
